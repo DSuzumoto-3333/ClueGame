232278f1e32019736c731d0dfe578c665c12d64c
Derek Suzumoto <dsuzumoto@mymail.mines.edu>
2021-02-23 18:54:43 -0700

    Added stub methods to TestBoard and TestBoardCell

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,28 @@
 package experiment;
 
-public class TestBoard {
+import java.util.*;
+import java.util.Set;
+import java.util.HashSet;
 
+public class TestBoard {
+	public static final int BOARD_WIDTH = 4;
+	public static final int BOARD_HEIGHT = 4;
+	private TestBoardCell[][] BOARD_LAYOUT;
+	
+	public TestBoard() {
+		super();
+	}
+	
+	public void calcTargets(TestBoardCell startCell, int length) {
+		return;
+	}
+	
+	public Set<TestBoardCell> getTargets(){
+		HashSet<TestBoardCell> t = new HashSet<TestBoardCell>();
+		return t;
+	}
+	
+	public TestBoardCell getCell(int row, int col) {
+		return null;
+	}
 }
--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,45 @@
 package experiment;
 
-public class TestBoardCell {
+import java.util.Set;
+import java.util.HashSet;
 
+public class TestBoardCell {
+	private int rowPos; 
+	private int colPos;
+	private boolean isOccupied;
+	private boolean isInRoom;
+	private Set<TestBoardCell> adjacentCells;
+	
+	public TestBoardCell(int rowPos, int colPos) {
+		super();
+		this.rowPos = rowPos;
+		this.colPos = colPos;
+	}
+	
+	public void addAdjacency(TestBoardCell cell) {
+		adjacentCells.add(cell);
+	}
+	
+	public Set<TestBoardCell> getAdjList(){
+		return adjacentCells;
+	}
+	
+	public void setInRoom(boolean isInRoom) {
+		this.isInRoom = isInRoom;
+	}
+	
+	public boolean getInRoom() {
+		return isInRoom;
+	}
+
+	public boolean getIsOccupied() {
+		return isOccupied;
+	}
+
+	public void setIsOccupied(boolean isOccupied) {
+		this.isOccupied = isOccupied;
+	}
+	
+	
+	
 }
