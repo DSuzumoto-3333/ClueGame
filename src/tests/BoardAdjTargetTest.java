8efbb02737460e19b963a5ef8101665911c71b2f
Luke Wakumoto <lukewakumoto@mymail.mines.edu>
2021-03-02 18:09:31 -0700

    Started working on the adjacency and target tests

--- src/tests/BoardAdjTargetTest.java
+++ src/tests/BoardAdjTargetTest.java
@@ -1,14 +1,48 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import gameEngine.Board;
+import gameEngine.BoardCell;
 
 class BoardAdjTargetTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private static Board board;
+
+	@BeforeAll
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+		// Initialize will load BOTH config files
+		board.initialize();
 	}
 
-}
+	
+	/*
+	 * These first few tests will test various adjacency logic and ensure that it works properly
+	 */
+	
+
+	// Ensure that player does not move around within room
+	// These cells are DARK GREEN on the planning spreadsheet
+		@Test
+		public void testAdjacenciesRooms()
+		{
+			// we want to test a couple of different rooms.
+			// First, the study that only has a single door but a secret room
+			Set<BoardCell> testList = board.getAdjList(5,1);
+			assertEquals(3, testList.size());
+
+		}
+	}
+
+
