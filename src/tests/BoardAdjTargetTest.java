75a23b1ca277f7376010a84411a8d1a937de1f98
Derek Suzumoto <dsuzumoto@mymail.mines.edu>
2021-03-02 19:32:49 -0700

    Added some BoardAdjTargetTest() methods, modiefied board.java to have dummy return values for getAdjList(). TODO: Merge with main and other methods.

--- src/tests/BoardAdjTargetTest.java
+++ src/tests/BoardAdjTargetTest.java
@@ -15,6 +15,9 @@ import gameEngine.BoardCell;
 class BoardAdjTargetTest {
 
 	private static Board board;
+	
+	public static final int WIDTH = 26;
+	public static final int HEIGHT = 22;
 
 	@BeforeAll
 	public static void setUp() {
@@ -52,6 +55,119 @@ class BoardAdjTargetTest {
 			assertTrue(testList.contains(board.getCell(11 , 8)));
 
 		}
+		
+		/**
+		 * Test the adjacency lists of tiles within rooms that are not the center tile. They should have empty adjacency lists.
+		 */
+		@Test
+		void testAdjacencyRoom() {
+			//Pick a random cell in the garden and get it's adjacency list.
+			Set<BoardCell> adj = board.getAdjList(4, 0);
+			//Assert that it's adjacency list has a size of 0.
+			assertEquals(0,adj.size());
+			
+			//Pick a random cell in the garden and get it's adjacency list.
+			adj = board.getAdjList(9, 0);
+			//Assert that it's adjacency list has a size of 0.
+			assertEquals(0,adj.size());
+			
+			//Pick a random cell in the garden and get it's adjacency list.
+			adj = board.getAdjList(17, 0);
+			//Assert that it's adjacency list has a size of 0.
+			assertEquals(0,adj.size());
+			
+			//Pick a random cell in the garden and get it's adjacency list.
+			adj = board.getAdjList(0, 12);
+			//Assert that it's adjacency list has a size of 0.
+			assertEquals(0,adj.size());
+			
+			//Pick a random cell in the garden and get it's adjacency list.
+			adj = board.getAdjList(9, 12);
+			//Assert that it's adjacency list has a size of 0.
+			assertEquals(0,adj.size());
+			
+			//Pick a random cell in the garden and get it's adjacency list.
+			adj = board.getAdjList(17, 12);
+			//Assert that it's adjacency list has a size of 0.
+			assertEquals(0,adj.size());
+			
+			//Pick a random cell in the garden and get it's adjacency list.
+			adj = board.getAdjList(24, 0);
+			//Assert that it's adjacency list has a size of 0.
+			assertEquals(0,adj.size());
+			
+			//Pick a random cell in the garden and get it's adjacency list.
+			adj = board.getAdjList(24, 9);
+			//Assert that it's adjacency list has a size of 0.
+			assertEquals(0,adj.size());
+			
+			//Pick a random cell in the garden and get it's adjacency list.
+			adj = board.getAdjList(24, 17);
+			//Assert that it's adjacency list has a size of 0.
+			assertEquals(0,adj.size());
+		}
+		
+		/**
+		 * Test to ensure that the adjacency lists of every tile on the edge of the board have a maximum of 3 tiles adjacent.
+		 */
+		@Test
+		void testAdjacenciesAtEdge() {
+			Set<BoardCell> left, right;
+			for(int i = 0; i < HEIGHT; i++) {
+				left = board.getAdjList(i, 0);
+				right = board.getAdjList(i, 25);
+				if(left.size() > 3 || right.size() > 3) {
+					fail("Invalid adjacency list");
+				}
+			}
+			for(int j = 0; j < WIDTH; j++) {
+				left = board.getAdjList(0, j);
+				right = board.getAdjList(22, j);
+				if(left.size() > 3 || right.size() > 3) {
+					fail("Invalid adjacency list");
+				}
+			}
+		}
+		
+		/**
+		 * Test to ensure that walkway tiles that are not doors cannot enter into rooms.
+		 */
+		@Test
+		void testAdjacencyOutsideRooms() {
+			//Pick a tile just outside of the parlor and below the kitchen.
+			Set<BoardCell> t = board.getAdjList(16, 3);
+			//It should have an adjacency list with 3 tiles, (15,3), (16,2), (16,4)
+			assertEquals(3, t.size());
+			assertTrue(t.contains(board.getCell(15, 3)));
+			assertTrue(t.contains(board.getCell(16, 2)));
+			assertTrue(t.contains(board.getCell(16, 4)));
+			
+			//Pick a tile just outside of the parlor to it's right.
+			t = board.getAdjList(15, 8);
+			//It should have an adjacency list with 3 tiles, (14,8), (16,8), (15,9)
+			assertEquals(3, t.size());
+			assertTrue(t.contains(board.getCell(14, 8)));
+			assertTrue(t.contains(board.getCell(16, 8)));
+			assertTrue(t.contains(board.getCell(15, 9)));
+			
+			//Pick a tile just above the pool, below the pantry.
+			t = board.getAdjList(8, 14);
+			//It should have an adjacency list with 3 tiles, (8,13), (8,15), (7,14)
+			assertEquals(3, t.size());
+			assertTrue(t.contains(board.getCell(8, 13)));
+			assertTrue(t.contains(board.getCell(8, 15)));
+			assertTrue(t.contains(board.getCell(7, 14)));
+			
+			//Pick a tile just outside of the Lounge, to the right of the Ballroom
+			t = board.getAdjList(19, 19);
+			//It should have an adjacency list with 3 tiles, (19,18), (18,19), (20,19)
+			assertEquals(3, t.size());
+			assertTrue(t.contains(board.getCell(19, 18)));
+			assertTrue(t.contains(board.getCell(18, 19)));
+			assertTrue(t.contains(board.getCell(20, 19)));
+		}
+		
+		
 	}
 
 
