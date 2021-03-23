4a4181ed4d0e3e7f83d4d4df2ada15d8d14a49e4
Derek Suzumoto <dsuzumoto@mymail.mines.edu>
2021-03-22 18:00:26 -0600

    Implemented code for disproveSuggestion(), all tests pass.

--- src/gameEngine/Player.java
+++ src/gameEngine/Player.java
@@ -4,10 +4,20 @@ import java.awt.Color;
 import java.util.Set;
 import java.util.HashSet;
 
+/**
+ * An abstract class to hold common methods and functions for HumanPlayer and ComputerPlayer objects.
+ * @author Derek Suzumoto
+ * @author Luke Wakumoto
+ *
+ */
 public abstract class Player {
+	//The player's current position
 	private int row, col;
+	//The player's name
 	private String name;
+	//A color to signify the player
 	private Color color;
+	//The player's hand, and the maximum number of cards they can hold, 3
 	private Set<Card> hand;
 	public static final int MAX_CARDS = 3;
 	
@@ -18,23 +28,65 @@ public abstract class Player {
 		hand = new HashSet<Card>();
 	}
 	
+	/**
+	 * Method for determining if the player has any card from a given suggestion in their hand, thus disproving the
+	 * suggestion. The program automatically returns the first match found, even if multiple matches are possible.
+	 * @param suggestion - A set of cards representing the suggestion
+	 * @return - The card held by the player that appears in the suggestion, or null.
+	 */
+	public Card disproveSuggestion(Set<Card> suggestion) {
+		//Compare each card in the player's hand to each card in the suggestion
+		for(Card handCard : hand) {
+			for(Card sugCard : suggestion) {
+				//If they are equal, return the match.
+				if (handCard.equals(sugCard)) {
+					return sugCard;
+				}
+			}
+		}
+		//If no match is found, return null.
+		return null;
+	}
+	
+	/**
+	 * A method to add a card to the player's hand.
+	 * @param card - The card to be added, assuming their hand is not already full.
+	 */
 	public void updateHand(Card card) {
 		if(!(hand.size() == MAX_CARDS)) {
 			hand.add(card);
 		}
 	}
 	
+	/**
+	 * A method to get the player's hand
+	 * @return - The player's hand.
+	 */
 	public Set<Card> getHand(){
 		return hand;
 	}
 	
+	/**
+	 * A method to get the player's name as a string.
+	 * @return - The player's name
+	 */
 	public String getName() {
 		return name;
 	}
-
+	
+	/**
+	 * A method to get the color assigned to the player
+	 * @return - The player's color as a java.awt.Color
+	 */
 	public Color getColor() {
 		return color;
 	}
-
+	
+	/**
+	 * A method to determine if one player is equal to another. Abstract since only HumanPlayer and ComputerPlayer
+	 * can be instantiated, and require different comparisons.
+	 * @param player - The player to be compared.
+	 * @return
+	 */
 	public abstract boolean equals (Player player);
 }
